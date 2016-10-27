a1a1a10f61c6317611a2ec92458e214763429a5d
Kyle Balisacan <kylebali@hawaii.edu>
2016-10-26 21:31:15 -1000

    finished home, landing, & login pages

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,6 +2,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/home', {
   name: 'Home_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Home_Page' });
@@ -19,13 +26,6 @@ FlowRouter.route('/login', {
   name: 'Login_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Login_Page' });
-  },
-});
-
-FlowRouter.route('/signup', {
-  name: 'Sign_Up_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'Sign_Up_Page' });
   },
 });
 
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -4,14 +4,13 @@ import './add-stuff-page.js';
 import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
+import './landing-page.html';
+
 import './home-page.html';
 import './home-page.js';
 
 import './login-page.html';
 import './login-page.js';
-
-import './signup_page.html';
-import './signup_page.js';
 
 import './profile_page.html';
 import './profile_page.js';
